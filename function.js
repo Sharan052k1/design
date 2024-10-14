c47067489ec13058030be36f5b6ca5110f02d87d
sharan <bsharan205@gmail.com>
2024-10-14 12:42:07 +0530

    main -"first commit"

---
+++ function.js
@@ -0,0 +1,19 @@
+/* // function declaration
+function example(){
+    console.log("function declaration")
+}
+example()
+// function erpression
+var a = function example(){
+    console.log("function expression")
+}
+a()
+// arrow function 
+const a =()=>{
+    console.log("arrow function")
+}
+example() */
+console.log(a);
+console.log(example());
+var a=10;
+var example = \
